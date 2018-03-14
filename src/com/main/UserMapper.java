19dbbb56d79f31882218064f56a19685b0758d96
Li GuoYao <gyli@utstar.com>
2018-03-14 22:38:04 +0800

    src && pom

---
+++ src/com/main/UserMapper.java
@@ -0,0 +1,23 @@
+package com.main;
+
+import java.util.List;
+
+public interface UserMapper {
+    //根据id查询用户信息
+    public User findUserById(int id) throws Exception;
+
+    //根据用户名列查询用户列表
+    public List<User> findUserByName(String name) throws Exception;
+
+    //添加用户信息
+    public void insertUser(User user) throws Exception;
+
+    //删除用户信息
+    public boolean deleteUser(int id) throws Exception;
+
+    //更新用户
+    public boolean updateUser(User user) throws Exception;
+
+    //根据id查询用户信息，使用resultMap输出
+    public User findUserByIdResultMap(int id) throws Exception;
+}
---
+++ src/test/TestMain.java
@@ -0,0 +1,71 @@
+package test;
+
+import com.main.User;
+import com.main.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+
+public class TestMain {
+    private String user = "root";
+    private String password = "";
+    private String url = "jdbc:mysql://localhost:3306/mybatis001";
+
+    private SqlSessionFactory sqlSessionFactory;
+    private SqlSession sqlSession;
+
+
+    @Before
+    public void beforeRun() {
+        String resource = "com/resources/mybatis/mybatisConfig.xml";
+        InputStream inputStream = null;
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        sqlSession = sqlSessionFactory.openSession();
+    }
+
+    @After
+    public void afterRun() {
+        sqlSession.close();
+    }
+
+    @Test
+    public void runA() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from user");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1));
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void runB() {
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        try {
+            User user = userMapper.findUserById(1);
+            System.out.println(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
