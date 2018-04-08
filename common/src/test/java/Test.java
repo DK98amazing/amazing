ff961f23ac01e6337da5a88cd427df33db18e432
Li GuoYao <gyli@utstar.com>
2018-04-08 18:25:25 +0800

    2018/4/8

--- common/src/test/java/Test.java
+++ common/src/test/java/Test.java
@@ -11,33 +11,33 @@ import java.util.regex.Pattern;
 //java8函数式编程
 public class Test {
     public static void main(String args[]) {
-//        Function<String, Integer> function = (String str) -> {return str.length();};
-//        Supplier<String> supplier = () -> {return "sd";};
-//        Predicate<String> predicate = (String str) -> {return str.length() == 5;};
-//        Predicate<String> predicate1 = (String str) -> {return str instanceof String;};
-//        Consumer<String> consumer = (String str) -> {System.out.println(str.length());};
-//        System.out.println(function.apply("sadadada"));
-//        System.out.println(supplier.get());
-//        System.out.println(predicate.test("12345"));
-//        System.out.println(predicate.and(predicate1).test("1234"));
-//        consumer.accept("ssssss");
-//        Collection<String> collection = new ArrayList<>();
-//        collection.add("123");collection.add("12345");
-//        collection.removeIf(predicate);
-//        System.out.println(collection.toArray().length);
+        Function<String, Integer> function = (String str) -> {return str.length();};
+        Supplier<String> supplier = () -> {return "sd";};
+        Predicate<String> predicate = (String str) -> {return str.length() == 5;};
+        Predicate<String> predicate1 = (String str) -> {return str instanceof String;};
+        Consumer<String> consumer = (String str) -> {System.out.println(str.length());};
+        System.out.println(function.apply("sadadada"));
+        System.out.println(supplier.get());
+        System.out.println(predicate.test("12345"));
+        System.out.println(predicate.and(predicate1).test("1234"));
+        consumer.accept("ssssss");
+        Collection<String> collection = new ArrayList<>();
+        collection.add("123");collection.add("12345");
+        collection.removeIf(predicate);
+        System.out.println(collection.toArray().length);
 
-//        String pattern = "^vc4=1|4|16$";
-//        Pattern r = Pattern.compile(pattern);
-//        Matcher m = r.matcher("vc4=1");
-//        System.out.println(m.matches());
+        String pattern = "^vc4=1|4|16$";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher("vc4=1");
+        System.out.println(m.matches());
 
         String content = "vc4=16\\vc12=1";
         String content1 = "vc4=16";
         String content2 = "vc12=9";
-        String pattern = "vc4=(1|4|16|64)\\\\vc12=(([1-5][0-9]?)|(6[0-3]?)|7|8|9)";
+        String pattern4 = "vc4=(1|4|16|64)\\\\vc12=(([1-5][0-9]?)|(6[0-3]?)|7|8|9)";
         String pattern1 = "vc4=(1|4|16|64)";
         String pattern2 = "vc12=(([1-5][0-9]?)|(6[0-3]?)|7|8|9)";
-        boolean isMatch = Pattern.matches(pattern, content);
+        boolean isMatch = Pattern.matches(pattern4, content);
         System.out.println(isMatch);
 
     }
