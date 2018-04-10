71988a5281ca5ed2bca0d0dbd3e1903b057a38d6
Li GuoYao <gyli@utstar.com>
2018-04-10 20:45:19 +0800

    并行

--- amazing-api/src/test/java/MyActorRef.java
+++
@@ -1,9 +0,0 @@
-import akka.actor.UntypedActor;
-
-public class MyActorRef extends UntypedActor {
-    public void onReceive(Object message) throws Throwable {
-        if (message instanceof java.lang.String) {
-            System.out.println(message + " is receive");
-        }
-    }
-}
