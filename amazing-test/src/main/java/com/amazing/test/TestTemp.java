dfdc3ab069f0295a951106474a4ca64af1b48329
amazing <1254770191@qq.com>
2019-08-01 09:54:46 +0800

    d

--- amazing-test/src/main/java/com/amazing/test/TestTemp.java
+++ amazing-test/src/main/java/com/amazing/test/TestTemp.java
@@ -1,12 +1,10 @@
 package com.amazing.test;
 
-import org.apache.commons.configuration2.PropertiesConfiguration;
-import org.apache.commons.configuration2.ex.ConfigurationException;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 
 /**
  * TestTemp.
@@ -14,10 +12,8 @@ import java.io.InputStreamReader;
  * @author liguoyao
  */
 public class TestTemp {
-    public static void main(String[] args) throws IOException, ConfigurationException {
-        PropertiesConfiguration propertiesConfiguration = new PropertiesConfiguration();
-        propertiesConfiguration.read(new InputStreamReader(new FileInputStream(new File("D:\\Application\\VNote\\noteRoot\\ROOT\\new_note_001.md"))));
-        System.err.println(propertiesConfiguration.getHeader());
+    public static void main(String[] args) throws IOException {
+        System.out.println(InetAddress.getLocalHost().getHostAddress());
     }
 
     private static void get() {
--- common/src/main/java/com/jedis/RedisClient.java
+++ common/src/main/java/com/jedis/RedisClient.java
@@ -12,9 +12,10 @@ import java.util.concurrent.TimeUnit;
 public class RedisClient {
     public static void main(String args[]) throws InterruptedException {
         Jedis jedis = new Jedis("10.0.11.134", 6379);
+        jedis.auth("123456");
 //        System.err.println(jedis.get("testKey22"));
 //        jedis.set("testKey22", "testValue22");
 //        System.err.println(jedis.get("testKey22"));
-        TimeUnit.SECONDS.sleep(2);
+        System.err.println(jedis.dbSize());
     }
 }
